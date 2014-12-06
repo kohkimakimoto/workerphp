71d9cbfee805accd33d224b99591cb6bafe2ce27
kohkimakimoto <kohki.makimoto@gmail.com>
2014-12-06 11:27:35 +0900

    add tests

--- tests/Kohkimakimoto/Worker/WorkerTest.php
+++ tests/Kohkimakimoto/Worker/WorkerTest.php
@@ -7,5 +7,7 @@ class WorkerTest extends \PHPUnit_Framework_TestCase
 {
     public function testDefault()
     {
+        $worker = new Worker();
+        $worker->job("* * * * *", "echo this is test");
     }
 }
