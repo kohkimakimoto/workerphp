1e63d6ea309e210d802a8ad0166328c5c20f8f81
kohkimakimoto <kohki.makimoto@gmail.com>
2014-12-10 20:03:13 +0900

    update

--- demo/worker.php
+++ demo/worker.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Asia/Tokyo');
 // $worker = new \Kohkimakimoto\Worker\Worker(["debug" => true]);
 $worker = new \Kohkimakimoto\Worker\Worker();
 
-$worker->httpServer("8888", "localhost");
+//$worker->httpServer("8888", "localhost");
 
 $worker->job("uptime", ['cron_time' => '* * * * *', 'command' => "uptime"]);
 $worker->job("hello", ['cron_time' => '* * * * *', 'max_processes' => 1, 'command' => function(){
