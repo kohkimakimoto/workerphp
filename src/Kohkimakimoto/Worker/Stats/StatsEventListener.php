65eed6996888252c273539e1ce1bad7a45ff75e1
kohkimakimoto <kohki.makimoto@gmail.com>
2014-12-16 07:43:13 +0900

    update

--- src/Kohkimakimoto/Worker/Stats/StatsEventListener.php
+++ src/Kohkimakimoto/Worker/Stats/StatsEventListener.php
@@ -15,16 +15,11 @@ class StatsEventListener implements EventSubscriberInterface
             $worker->eventLoop->addPeriodicTimer($worker->stats->getInterval(), function () use ($worker) {
 
                 $uptime = $worker->stats->getUptime();
-
-                if ($uptime > 60) {
-                    $uptime = round(($uptime / 60), 0)." minutes";
-                } else {
-                    $uptime = $uptime." seconds";
-                }
+                $uptime = gmdate("H:i:s", $uptime);
 
                 $mem = memory_get_usage();
                 $memM = round(($mem / 1024 / 1024), 1);
-                $worker->output->writeln("<info>Stats report:</info> memory_usage: <comment>$mem</comment> bytes ($memM MB). uptime: $uptime. at ".(new \DateTime())->format('Y-m-d H:i:s'));
+                $worker->output->writeln("<info>Stats report:</info> memory_usage: <comment>$mem</comment> bytes ($memM MB). uptime: <comment>$uptime</comment>. at ".(new \DateTime())->format('Y-m-d H:i:s'));
 
             });
         }
